cecf8a1c1aeab03cfb2015ae322da27333055c29
KirillReal <kiril5471710@gmail.com>
2021-05-05 02:53:36 +0300

    2. Создать TODO list [#490599]

---
+++ db/scripts/update_001.sql
@@ -0,0 +1,6 @@
+CREATE TABLE item (
+                      id SERIAL PRIMARY KEY,
+                      description TEXT NOT NULL,
+                      created TIMESTAMP NOT NULL DEFAULT now(),
+                      done BOOLEAN NOT NULL DEFAULT false
+);
